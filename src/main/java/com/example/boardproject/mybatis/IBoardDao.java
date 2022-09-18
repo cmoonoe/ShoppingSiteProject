743e18e357c71ae56246d6a52a5ff3a771de956f
Choi moon yeong <cmoonoe@gmail.com>
2022-09-18 17:30:22 +0900

    shopping site update v1.2

---
+++ src/main/java/com/example/boardproject/mybatis/IBoardDao.java
@@ -0,0 +1,15 @@
+package com.example.boardproject.mybatis;
+
+import com.example.boardproject.entity.Board;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface IBoardDao {
+
+    @Select("select bId, bTitle, bWriter, bDate from Board")
+    List<Board> list();
+
+}
--- src/main/java/com/example/boardproject/service/ShowServiceImpl.java
+++ src/main/java/com/example/boardproject/service/ShowServiceImpl.java
@@ -18,13 +18,28 @@ public class ShowServiceImpl implements ShowService{
         this.boardService = boardService;
     }
 
+    /* bid, pid로 board 찾기 */
     @Override
     public BoardDTO getByBId(int bId, int pId) {
 
         BoardDTO boardDTO = boardService.entityToDto(showRepository.findByBId(bId).get());
         ProductDTO productDTO = ProductDTO.entityToDto(showRepository.findByPId(pId).get());
 
-        /* 리뷰 사진이 없으면 상품 사진으로 대체 */
+        boardDTO = getReviewImage(boardDTO, productDTO);
+        boardDTO = getAsteriskWriter (boardDTO);
+
+        return boardDTO;
+    }
+
+    /* pid로 product 찾기 */
+    @Override
+    public ProductDTO getByPId(int pId) {
+        return ProductDTO.entityToDto(showRepository.findByPId(pId).get());
+    }
+
+    /* 리뷰 사진이 없으면 상품 사진으로 대체 */
+    private BoardDTO getReviewImage (BoardDTO boardDTO, ProductDTO productDTO) {
+
         if (boardDTO.getBImage() == null) {
             boardDTO.setBImage(productDTO.getPImage());
             return boardDTO;
@@ -33,9 +48,21 @@ public class ShowServiceImpl implements ShowService{
         }
     }
 
-    @Override
-    public ProductDTO getByPId(int pid) {
-        return ProductDTO.entityToDto(showRepository.findByPId(pid).get());
+    /* 상세 리뷰 보기에서 작성자 이름 일부만 표시하기 */
+    private BoardDTO getAsteriskWriter (BoardDTO boardDTO) {
+        String bWriter = boardDTO.getBWriter();
+
+        String AsteriskWriter = null;
+
+        if (bWriter.length() < 4) {
+            AsteriskWriter = bWriter.replace(bWriter.substring(bWriter.length()-1), "*");
+        } else {
+            AsteriskWriter = bWriter.replace(bWriter.substring(bWriter.length()-3), "***");
+        }
+
+        boardDTO.setBWriter(AsteriskWriter);
+
+        return boardDTO;
     }
 }
 
