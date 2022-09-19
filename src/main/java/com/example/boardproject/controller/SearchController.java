9b22f1b4949bfa1ab391660e5d5feb78a5805369
Choi moon yeong <cmoonoe@gmail.com>
2022-09-19 12:02:02 +0900

    shopping site update v1.5

--- src/main/java/com/example/boardproject/controller/SearchController.java
+++ src/main/java/com/example/boardproject/controller/SearchController.java
@@ -1,34 +1,27 @@
 package com.example.boardproject.controller;
 
-import com.example.boardproject.dto.PageRequestDTO;
 import com.example.boardproject.repository.SearchRepository;
-import com.example.boardproject.service.BoardService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class SearchController {
-
-    @Autowired
-    SearchRepository searchRepository;
-
-    @Autowired
-    BoardService boardService;
-
+    private final SearchRepository searchRepository;
     @GetMapping("/")
-    public String main(Model model) {
+    String main(Model model) {
         model.addAttribute("productList", searchRepository.productList());
         return "main";
     }
 
     @GetMapping("/product/{pId}")
-    public String productList(@PathVariable int pId, PageRequestDTO pageRequestDTO, Model model) {
-        model.addAttribute("lists", boardService.getList(pId,pageRequestDTO));
+    String showContents4(@PathVariable Long pId, Model model) {
+        model.addAttribute("allContents", searchRepository.searchList(pId));
+        model.addAttribute("pId", pId);
         return "list";
+
     }
-
-
 }
--- src/main/java/com/example/boardproject/controller/WriteController.java
+++ src/main/java/com/example/boardproject/controller/WriteController.java
@@ -1,4 +1,64 @@
 package com.example.boardproject.controller;
+/**
+ * created : OH
+ * last update : 2022.09.16
+ */
 
+import com.example.boardproject.domain.UploadFile;
+import com.example.boardproject.dto.BoardDTO;
+import com.example.boardproject.entity.Board;
+import com.example.boardproject.entity.Product;
+import com.example.boardproject.repository.ProductRepository;
+import com.example.boardproject.repository.WriteRepository;
+import com.example.boardproject.service.FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
 public class WriteController {
+
+    private final WriteRepository writeRepository;
+    private final FileService fileService;
+    private final ProductRepository productRepository;
+
+    @GetMapping("/write/{pId}")
+    public String writeForm(@PathVariable Long pId, Model model) {
+        model.addAttribute("pId",pId);
+        return "write";
+    }
+
+    @PostMapping("/write/{pId}")
+    public String saveBoard(@ModelAttribute BoardDTO boardDTO,
+                            RedirectAttributes redirectAttributes,
+                            @PathVariable Long pId) throws IOException {
+
+        Optional<Product> result = productRepository.findById(pId);
+        Product product = result.get();
+
+        List<UploadFile> storeImageFiles =
+                fileService.storeFiles(boardDTO.getPImageFiles());
+
+        Board board = new Board(boardDTO.getBTitle(),
+                boardDTO.getBWriter(),
+                boardDTO.getBContent(),
+                storeImageFiles,
+                boardDTO.getBPw(),
+                product);
+
+        writeRepository.save(board);
+//
+//        redirectAttributes.addAttribute("bId", board.getBId());
+        redirectAttributes.addFlashAttribute("bId", board.getBId());
+
+        return "redirect:/";
+    }
 }
