40240b6ab2b83ec7b80c0179451f9393fde607b5
Choi moon yeong <cmoonoe@gmail.com>
2022-09-19 10:24:30 +0900

    shopping site update v1.2

--- src/main/java/com/example/boardproject/controller/ListController.java
+++ src/main/java/com/example/boardproject/controller/ListController.java
@@ -6,8 +6,6 @@ import com.example.boardproject.service.BoardService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
