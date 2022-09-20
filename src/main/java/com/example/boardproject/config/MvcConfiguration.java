a4f2fe69fb536ae0a0ab91011bff8a291b220e1e
Choi moon yeong <cmoonoe@gmail.com>
2022-09-20 10:53:29 +0900

    shopping site show3 update v1.13

---
+++ src/main/java/com/example/boardproject/config/MvcConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.boardproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //TODO [resources >> static >> 리소스 연결]
+
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:C://JavaProgramming/_project/myImage/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+
+    }
+}
